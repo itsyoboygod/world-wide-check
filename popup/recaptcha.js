8c2edb4ce3d4175261ccfd5221be18683bf6635e
itsyoboygod <you@example.com>
2025-02-27 11:18:24 -0300

    Reset gh-pages with clean index.html

--- popup/recaptcha.js
+++
@@ -1,7 +0,0 @@
-function verifyCallback(response) {
-    if (response) {
-        // Send success message back to the main extension
-        window.opener.postMessage("recaptchaSuccess", "*");
-        window.close();
-    }
-}
